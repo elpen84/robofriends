60d64814629f772eb87039c91ace4dc261cee658
elpen84 <elplist@gmail.com>
2020-01-01 23:19:07 -0800

    Updates

---
+++ precache-manifest.83d2882f26508870d2a18ca35b4dee70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfed03148c0302ba7269f0980bfcf9eb",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7423dc8ccc655cf5f964",
+    "url": "/robofriends/static/css/main.0ff520dc.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "7423dc8ccc655cf5f964",
+    "url": "/robofriends/static/js/main.1f9e78bb.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
